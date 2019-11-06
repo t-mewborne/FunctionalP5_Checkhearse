089fa677c27f52e575f4eccc8c4910306b26cecd
Travis Mewborne <tmewborn@trinity.edu>
2019-11-06 12:22:50 -0600

    worked on buildBoard:

--- Checkhearse.hs
+++ Checkhearse.hs
@@ -9,14 +9,36 @@ data Player = Black | Red
 data Piece = Reg Player | King Player | Empty
 
 
-type Loc = (Int,Int)
+type Loc = (Int,Int) --(row,column)
 type Square = (Loc,Piece)
-type Board = [[Piece]]
---type Board = [Square]
-type Game = (Board,Player)
+--type Board = [[Piece]]
+type Board = [Square] --choose between these two types for board (only contains playable spaces)
+type Game = (Board,Player) --player = current turn
 
 -- type Board = [Square] (2nd Declare)
 type Move = ((Char,Int),(Char,Int)) --((Start),(End),Turn)
+
+buildGame :: Game
+buildGame = 
+    let allRows = [1..8]
+        allColumns = [1..8]
+
+        
+
+        --bd = [((1,2),Reg Black),
+        --      ((
+        bd = buildRow 1 2 Black
+        buildRows :: --odd -> start at 2
+                     --even -> start at 1
+                     --stop after 8 and jump to 6 after 3
+                     --increment by 1
+
+        buildRow :: Int -> Int -> Player -> Board --build one row
+        buildRow row column plyr
+            | column >= 9 = []
+            | otherwise = ((row,column),Reg plyr):(buildRow row (column + 2) plyr)
+    
+    in (bd, Black)
 
 showBoard :: Game ->  [String] --One string per row
 showBoard = undefined
