dc3fcadb32dec18fa245708dae4588cecff6e4f3
Travis Mewborne <tmewborn@trinity.edu>
2019-11-06 17:35:46 -0600

    fixed buildGame

--- Checkhearse.hs
+++ Checkhearse.hs
@@ -5,8 +5,8 @@ import Data.Tuple (swap)
 import Data.Ratio (numerator, denominator)
 import Data.Ratio ((%))
 
-data Player = Black | Red
-data Piece = Reg Player | King Player | Empty
+data Player = Black | Red deriving Show
+data Piece = Reg Player | King Player | Empty deriving Show
 
 
 type Loc = (Int,Int) --(row,column)
@@ -18,30 +18,24 @@ type Game = (Board,Player) --player = current turn
 -- type Board = [Square] (2nd Declare)
 type Move = ((Char,Int),(Char,Int)) --((Start),(End),Turn)
 
-buildGame :: Game
+buildGame :: Game --Initial State of the board
 buildGame = 
-    let allRows = [1..8]
-        allColumns = [1..8]
-
-        
-
-        --bd = [((1,2),Reg Black),
-        --      ((
-        bd = buildRow 1 2 Black
-        buildRows :: --odd -> start at 2
-                     --even -> start at 1
-                     --stop after 8 and jump to 6 after 3
-                     --increment by 1
-
+    let bd = buildRow 1 2 Black ++
+             buildRow 2 1 Black ++
+             buildRow 3 2 Black ++
+             buildRow 4 1 Empty ++
+             buildRow 5 2 Empty ++
+             buildRow 6 1 Red ++
+             buildRow 7 2 Red ++
+             buildRow 8 1 Red
         buildRow :: Int -> Int -> Player -> Board --build one row
         buildRow row column plyr
             | column >= 9 = []
             | otherwise = ((row,column),Reg plyr):(buildRow row (column + 2) plyr)
-    
     in (bd, Black)
 
 showBoard :: Game ->  [String] --One string per row
-showBoard = undefined
+showBoard game = undefined
 
 updateBoard :: Game -> Move -> Maybe Board --is the move valid? Should we make this a return a Maybe Game?
 updateBoard = undefined
